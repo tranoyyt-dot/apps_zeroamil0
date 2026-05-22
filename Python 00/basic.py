c5492d47c035ca39eb64aa9080fef730fead7319
Arthur <tranoyyt@gmail.com>
2026-05-22 07:33:44 -0300

    Create basic.py

---
+++ Python 00/basic.py
@@ -0,0 +1,16 @@
+print('hello peoples!')
+# snake case = name_he
+# camel case = namehe
+nameteacher = 'Jeronimo'
+yearsteacher = 18
+
+# input = information entry
+# output = information leave
+print("Python leave to the Terminal")
+user0 = input("Enter your Name:")
+print("Hello,", user0)
+
+# print receives the operations
+num1 = 20
+num2 = 35
+print('Sum: ', num1 + num2)
